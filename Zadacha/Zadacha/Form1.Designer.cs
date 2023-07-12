651bf159c02c1bd36b58864adb05d40b7b352d96
zimbloe <139367941+zimbloe@users.noreply.github.com>
2023-07-12 18:28:03 +0300

    Add files via upload

---
+++ Zadacha/Zadacha/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Zadacha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(36, 331);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(92, 36);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(601, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCustomerID.Location = new System.Drawing.Point(77, 55);
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.Size = new System.Drawing.Size(188, 24);
+            this.txtCustomerID.TabIndex = 2;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCompanyName.Location = new System.Drawing.Point(77, 251);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(293, 22);
+            this.txtCompanyName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(77, 202);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(293, 22);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleName.Location = new System.Drawing.Point(77, 152);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(293, 22);
+            this.txtMiddleName.TabIndex = 5;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(77, 102);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(293, 22);
+            this.txtFirstName.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrevious);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtFirstName;
+    }
+}
+
---
+++ Zadacha/Zadacha/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Zadacha
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source = local; Initial Catalog=AdventureWorks;Integrated Security=True";
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        private int currentRowIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection  = new SqlConnection(connectionString);
+                connection.Open();
+                command = new SqlCommand("Select CustomerID, FirstName, MiddleName, LastName, CompanyName FROM SalesLT.Customer ORDER BY CompanyName", connection);
+                reader = command.ExecuteReader();
+
+                if(reader.HasRows)
+                {
+                    reader.Read();
+                    DisplayCustomerData();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greshka pri svurzvaneto s bazata danni" + ex.Message);
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if(currentRowIndex > 0 )
+            {
+                currentRowIndex--;
+                DisplayCustomerData();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(reader != null && reader.HasRows && currentRowIndex < reader.FieldCount - 1)
+            {
+                currentRowIndex++;
+                DisplayCustomerData();
+            }
+        }
+        
+        private void DisplayCustomerData()
+        {
+            txtCustomerID.Text = reader.GetInt32(0).ToString();
+            txtFirstName.Text = reader.GetString(1);
+            txtMiddleName.Text = reader.GetString(2);
+            txtLastName.Text = reader.GetString(3);
+            txtCompanyName.Text = reader.GetString(4);
+        }
+
+        private void Form1_Load_Closing(object sender, FormClosedEventArgs e)
+        {
+            if(reader != null)
+            {
+                reader.Close();
+            }
+
+            if(connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
